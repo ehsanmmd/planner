90e28b6635a335643645b15c2f912419db021466
ehsanmmd <e.mahmoudi.a@gmail.com>
2022-11-01 00:47:27 +0330

    feat: added calendar picker

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
 import * as React from "react";
+import dayjs from "dayjs";
+import { CalendarPicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { Grid } from "@mui/material";
 
 export default function Home() {
-  return <></>;
+  const [date, setDate] = React.useState(dayjs("2022-04-07"));
+
+  return (
+    <>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={6}>
+            <CalendarPicker
+              date={date}
+              onChange={(newDate) => setDate(newDate)}
+              maxDate="2024-01-01"
+              minDate="2018-01-01"
+            />
+          </Grid>
+        </Grid>
+      </LocalizationProvider>
+    </>
+  );
 }
