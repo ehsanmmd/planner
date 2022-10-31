a6fd5952afe115ed23c509bc9d23d2e193c0c68d
ehsanmmd <e.mahmoudi.a@gmail.com>
2022-11-01 00:48:49 +0330

    feat: add buttom navigation

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,19 @@ import * as React from "react";
 import dayjs from "dayjs";
 import { CalendarPicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { Grid } from "@mui/material";
+import {
+  BottomNavigation,
+  BottomNavigationAction,
+  Grid,
+  Paper,
+} from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 export default function Home() {
   const [date, setDate] = React.useState(dayjs("2022-04-07"));
+  const [value, setValue] = React.useState(0);
 
   return (
     <>
@@ -21,6 +30,23 @@ export default function Home() {
           </Grid>
         </Grid>
       </LocalizationProvider>
+
+      <Paper
+        sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+        elevation={3}
+      >
+        <BottomNavigation
+          showLabels
+          // value={value}
+          // onChange={(event, newValue) => {
+          //   setValue(newValue);
+          // }}
+        >
+          <BottomNavigationAction icon={<HomeIcon fontSize="large" />} />
+          <BottomNavigationAction icon={<AddCircleIcon fontSize="large" />} />
+          <BottomNavigationAction icon={<SettingsIcon fontSize="large" />} />
+        </BottomNavigation>
+      </Paper>
     </>
   );
 }
