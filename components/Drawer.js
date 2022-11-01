c8d6881b4c5a8751e1cf551de889092af0a5fdfc
ehsanmmd <e.mahmoudi.a@gmail.com>
2022-11-01 21:51:24 +0330

    feat: added Drawer

---
+++ components/Drawer.js
@@ -0,0 +1,84 @@
+import CssBaseline from "@mui/material/CssBaseline";
+import { Global } from "@emotion/react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import SwipeableDrawer from "@mui/material/SwipeableDrawer";
+import { useState } from "react";
+import { grey } from "@mui/material/colors";
+
+const drawerBleeding = 56;
+
+export default function Drawer() {
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+  const [open, setOpen] = useState(true);
+  const StyledBox = styled(Box)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "light" ? "#fff" : grey[800],
+  }));
+  const Puller = styled(Box)(({ theme }) => ({
+    width: 30,
+    height: 6,
+    backgroundColor: theme.palette.mode === "light" ? grey[300] : grey[900],
+    borderRadius: 3,
+    position: "absolute",
+    top: 8,
+    left: "calc(50% - 15px)",
+  }));
+
+  return (
+    <>
+      <CssBaseline />
+      <Global
+        styles={{
+          ".MuiDrawer-root > .MuiPaper-root": {
+            height: `calc(50% - ${drawerBleeding}px)`,
+            overflow: "visible",
+          },
+        }}
+      />
+      {/* <Box sx={{ textAlign: "center", pt: 1 }}>
+        <Button onClick={toggleDrawer(true)}>Open</Button>
+      </Box> */}
+      <SwipeableDrawer
+        anchor="bottom"
+        open={open}
+        onClose={toggleDrawer(false)}
+        onOpen={toggleDrawer(true)}
+        swipeAreaWidth={drawerBleeding}
+        disableSwipeToOpen={false}
+        ModalProps={{
+          keepMounted: true,
+        }}
+        hideBackdrop={true}
+      >
+        <StyledBox
+          sx={{
+            position: "absolute",
+            top: -drawerBleeding,
+            borderTopLeftRadius: 30,
+            borderTopRightRadius: 30,
+            visibility: "visible",
+            right: 0,
+            left: 0,
+          }}
+        >
+          <Puller />
+          <Typography sx={{ p: 2, color: "text.secondary" }}>
+            Upcomming
+          </Typography>
+        </StyledBox>
+        <StyledBox
+          sx={{
+            px: 2,
+            pb: 2,
+            height: "100%",
+            overflow: "auto",
+          }}
+        ></StyledBox>
+      </SwipeableDrawer>
+    </>
+  );
+}
