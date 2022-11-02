bc11e5dab8dea186b441089dc224346844352810
ehsanmmd <e.mahmoudi.a@gmail.com>
2022-11-02 23:35:27 +0330

    fix: bring BottomNavigation up on the Drawer

--- components/BottomNav.js
+++ components/BottomNav.js
@@ -2,19 +2,23 @@ import { BottomNavigation, BottomNavigationAction, Paper } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import SettingsIcon from "@mui/icons-material/Settings";
+import { useState } from "react";
 
-export default function BottomNav() {
+export default function BottomNav(props) {
+  const [value, setValue] = useState(0);
+
   return (
     <Paper
-      sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+      sx={{ position: "fixed", bottom: 0, left: 0, right: 0, zIndex: 1300 }}
       elevation={6}
     >
       <BottomNavigation
         showLabels
-        // value={value}
-        // onChange={(event, newValue) => {
-        //   setValue(newValue);
-        // }}
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+          console.log(newValue);
+        }}
       >
         <BottomNavigationAction icon={<HomeIcon fontSize="large" />} />
         <BottomNavigationAction icon={<AddCircleIcon fontSize="large" />} />
